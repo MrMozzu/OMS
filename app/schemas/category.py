2396f2ab56a02e2477ec741092177865e03fe516
MrMozzu <muzammilahsan07@gmail.com>
2026-07-27 13:29:45 +0530

    added pydantic schema validation

---
+++ app/schemas/category.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+class CategoryCreate(BaseModel):
+    
+    name: str = Field(
+        min_length=3,
+        max_length=40
+    )
+
+
+class CateogoryUpdate(BaseModel):
+
+        name: str | None = Field(
+            default=None,
+            min_length=2,
+            max_length=40
+        )
+
+
+class CategoryResponse(BaseModel):
+
+    model_config = ConfigDict(
+       from_attributes=True
+    )
+
+    id: int
+    name: str
